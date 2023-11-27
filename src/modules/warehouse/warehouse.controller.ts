b7d27cd60ad5443ac19ec6d7459cf7e7ebe7e844
Guuhp <lg645471@gmail.com>
2023-11-26 20:59:44 -0400

    (Feat) created the warehouse creation and update method

--- src/modules/warehouse/warehouse.controller.ts
+++ src/modules/warehouse/warehouse.controller.ts
@@ -11,19 +11,23 @@ import { CreateWarehouseDto } from './dto/create-warehouse.dto';
 import { UpdateWarehouseDto } from './dto/update-warehouse.dto';
 import { ApiTags } from '@nestjs/swagger';
 import { RoleGuard } from 'src/guards/role.guard';
+import { Roles } from 'src/decorators/role.decorator';
+import { Role } from 'src/enums/role.enum';
+import { AuthVerify } from 'src/guards/authVerify.guard';
 
 @ApiTags('warehouse')
-@UseGuards(RoleGuard)
+@UseGuards(RoleGuard, AuthVerify)
 @Controller('warehouse')
 export class WarehouseController {
   constructor(private readonly warehouseService: WarehouseService) {}
 
-  //@Roles(Role.ADMIN)
+  @Roles(Role.ADMIN)
   @Post()
   create(@Body() createWarehouseDto: CreateWarehouseDto) {
     return this.warehouseService.create(createWarehouseDto);
   }
 
+  @Roles(Role.ADMIN)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateWarehouse: UpdateWarehouseDto) {
     return this.warehouseService.update(id, updateWarehouse);
