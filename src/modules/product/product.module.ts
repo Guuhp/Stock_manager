de9595c5afe06d42fdc6d02f88fa783a3111ab72
Guuhp <lg645471@gmail.com>
2024-03-04 10:21:53 -0400

    (Feat) created method createProduct in Products

--- src/modules/product/product.module.ts
+++ src/modules/product/product.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { ProductController } from './product.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { WarehouseModule } from '../warehouse/warehouse.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, WarehouseModule],
   controllers: [ProductController],
   providers: [ProductService],
 })
--- src/modules/product/product.service.ts
+++ src/modules/product/product.service.ts
@@ -1,29 +1,90 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { NotFoundException } from 'src/exceptions/expection';
+import { WarehouseService } from '../warehouse/warehouse.service';
 
 @Injectable()
 export class ProductService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly warehouseService: WarehouseService,
+  ) {}
+
+  generateSku(name, code, brand) {
+    const codeI = String(code);
+    const sku = `${name}-${brand}-${codeI}`;
+    return sku;
+  }
+
+  generateBarCode() {
+    const numerosAleatorios = Array(13).fill(null);
+    numerosAleatorios.forEach((_, i) => {
+      numerosAleatorios[i] = Math.floor(Math.random() * 10);
+    });
+    const codigoAleatorio = numerosAleatorios.join('');
+    return codigoAleatorio;
+  }
 
   async findAll() {
     const users = await this.prisma.product.findMany({
-      include: {
-        category: true,
-        //warehouses: true,
-        //ProductSupplier: true,
-        //AssociateWarehouseProduct: true
-      },
+      //     include: {
+      //       category: true,
+      //       warehouses: true,
+      //       ProductSupplier: true,
+      //       AssociateWarehouseProduct: true,
+      //     },
     });
     return users;
   }
 
-  async create(data: CreateProductDto) {
-    console.log(data);
-    const product = this.prisma.product.create({
+  async update(id: string, data: UpdateProductDto) {
+    const existsUser = await this.prisma.product.findFirst({ where: { id } });
+
+    if (!existsUser) throw new NotFoundException('user');
+
+    const userUpdate = await this.prisma.product.update({
+      where: { id },
       data,
+    });
+    return userUpdate;
+  }
+
+  async create(data: CreateProductDto) {
+    let code = this.warehouseService.generateUniqueCode();
+    let sku = this.generateSku(data.name, code, data.brand);
+    let barCode = this.generateBarCode();
+
+    while (true) {
+      const [existsSku, existsBarCode] = await Promise.all([
+        await this.prisma.product.findUnique({
+          where: { sku },
+        }),
+        await this.prisma.product.findUnique({
+          where: { barCode },
+        }),
+      ]);
+
+      if (!existsSku && !existsBarCode) {
+        break;
+      }
+      code = this.warehouseService.generateUniqueCode();
+      sku = this.generateSku(data.name, code, data.brand);
+      barCode = this.generateBarCode();
+    }
+
+    const product = await this.prisma.product.create({
+      data: {
+        ...data,
+        sku,
+        barCode,
+      },
     });
 
     return product;
   }
+
+  //GERAR O QRCODE
+  //SCANEAR O QRCODE
 }
